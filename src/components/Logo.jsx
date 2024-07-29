870cebbcf005fe887111d88e5c8249008c6991e8
Muhammad Gad <106442148+GADMuhammad@users.noreply.github.com>
2024-07-30 01:30:19 +0300

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import logoImage from "/public/planet-outline.svg";
 
 const Logo = () => {
   return (
     <Link to="/" className="flex items-center gap-3">
-      <img src="/public/planet-outline.svg" width="24" height="28" />
+      <img src={logoImage} width="24" height="28" />
       <h3>Around the world</h3>
     </Link>
   );
